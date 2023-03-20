902804450b638aa887c407f4c6eec15e06f26703
Nishita <nishnikdutta@gmail.com>
2023-03-20 13:08:46 +0800

    MOdified setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,15 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-setup(name='taxi-rl-env',
-      version='0.0.1',
-      install_requires=['gym==0.26.2', 'numpy'],
-      packages=['taxi_rl_env']
+setup(
+    name="taxi_rl_env",
+    version="0.0.1",
+    description="Customised environment for Open AI Taxi",
+    url="https://github.com/cs4246/taxi-rl-env",
+    author="Nishita Dutta",
+    author_email="nishita.dutta@u.nus.edu",
+    packages=["taxi_rl_env"],
+    install_requires=["pyzmq", "gym"],
+    python_requires=">=3.4",
+    setup_requires=['wheel'],
+    zip_safe=False,
 )
